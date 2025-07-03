21fdf8344b5b46fe31ba586300a9d9838c382266
Gehadmh <122981427+Gehadmh@users.noreply.github.com>
2025-07-03 20:42:30 +0300

    init

---
+++ src/test/java/testPackage/TestClass.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+publiic class TestClass {
+    @Test
+
+}
