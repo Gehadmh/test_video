13b256f7170cef114cce323ea8b511c9cc8c22f7
Gehadmh <122981427+Gehadmh@users.noreply.github.com>
2025-07-11 01:53:00 +0300

    2 new methods #project still on going

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -2,9 +2,40 @@ package testPackage;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-publiic class TestClass {
-    @Test
 
-}
+    @Test
+public class TestClass{
+        WebDriver driver;
+
+        @BeforeMethod
+        public void beforeMethod() {
+            driver = new ChromeDriver();
+        }
+
+        @AfterMethod
+        public void afterMethod() {
+            driver.quit();
+        }
+        public void navigatetoduckduckgoandcheckpagetitle(){
+          //  driver = new ChromeDriver();
+            driver.get("https://duckduckgo.com/");
+            Assert.assertNotEquals(driver.getTitle(),"Google");
+          //  driver.quit();
+        }
+
+        public void navigatetoduckduckgoandasserpagetitle(){
+         //   driver = new ChromeDriver();
+            driver.get("https://duckduckgo.com/");
+            System.out.println("Page Title: " + driver.getTitle());
+         //   driver.quit();
+        }
+
+
+
+    }
+
